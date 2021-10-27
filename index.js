7532f999e1d84c87ec1c28fc83eefb6903149be7
Tonyrj3268 <rj3268@yahoo.com.tw>
2021-10-27 14:40:58 +0800

    make it better

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ app.post("/webhook", function(req, res) {
   if (req.body.events[0].type === "message") {
       // Message data, must be stringified
       const dataString = JSON.stringify({
-        replyToken: req.body.events[0].replyToken,
+      handleEvent(req.body.events[0]);
+        /*replyToken: req.body.events[0].replyToken,
         messages: [
           {
             "type": "text",
@@ -29,8 +30,214 @@ app.post("/webhook", function(req, res) {
             "type": "text",
             "text": "May I help you?"
           }
-        ]
+        ]*/
       })
+
+function handleEvent(event){
+if (event.replyToken && event.replyToken.match(/^(.)\1*$/)) {
+    return console.log('Test hook recieved: ' + JSON.stringify(event.message));
+  }
+  console.log(`User ID: ${event.source.userId}`)
+
+  switch(event.type){
+  case "message":
+  const message = event.message;
+        switch (message.type) {
+          case 'text':
+            return handleText(message, event.replyToken, event.source);
+          case 'image':
+            return handleImage(message, event.replyToken);
+          case 'video':
+            return handleVideo(message, event.replyToken);
+          case 'audio':
+            return handleAudio(message, event.replyToken);
+          case 'location':
+            return handleLocation(message, event.replyToken, event.source);
+          case 'sticker':
+            return handleSticker(message, event.replyToken);
+          default:
+            throw new Error(`Unknown message: ${JSON.stringify(message)}`);
+        }
+      default:
+        throw new Error(`Unknown event: ${JSON.stringify(event)}`);
+
+
+  }
+
+}
+
+function handleText(message, replyToken, source) {
+  switch (message.text) {
+    case '公車':
+        return client.replyMessage(replyToken, [
+          {
+            type: 'image',
+            originalContentUrl: 'https://kbus.com.tw/upload/ckeditor/images/%E7%9B%B4-E25-1100308.jpg',
+            previewImageUrl: 'https://developers.line.biz/media/messaging-api/messages/image-167efb33.png'
+          }
+        ]);
+    case '溫泉':
+        return client.replyMessage(replyToken,[
+        {
+                                                  "type": "bubble",
+                                                  "hero": {
+                                                    "type": "image",
+                                                    "url": "https://storage.googleapis.com/smiletaiwan-cms-cwg-tw/article/201901/article-5c3bfec68ea43.jpg",
+                                                    "size": "full",
+                                                    "aspectRatio": "20:13",
+                                                    "aspectMode": "cover",
+                                                    "action": {
+                                                      "type": "uri",
+                                                      "uri": "http://linecorp.com/"
+                                                    }
+                                                  },
+                                                  "body": {
+                                                    "type": "box",
+                                                    "layout": "vertical",
+                                                    "contents": [
+                                                      {
+                                                        "type": "text",
+                                                        "text": "寶來溫泉",
+                                                        "weight": "bold",
+                                                        "size": "xl"
+                                                      },
+                                                      {
+                                                        "type": "box",
+                                                        "layout": "baseline",
+                                                        "margin": "md",
+                                                        "contents": [
+                                                          {
+                                                            "type": "icon",
+                                                            "size": "sm",
+                                                            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+                                                          },
+                                                          {
+                                                            "type": "icon",
+                                                            "size": "sm",
+                                                            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+                                                          },
+                                                          {
+                                                            "type": "icon",
+                                                            "size": "sm",
+                                                            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+                                                          },
+                                                          {
+                                                            "type": "icon",
+                                                            "size": "sm",
+                                                            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+                                                          },
+                                                          {
+                                                            "type": "icon",
+                                                            "size": "sm",
+                                                            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png"
+                                                          },
+                                                          {
+                                                            "type": "text",
+                                                            "text": "4.0",
+                                                            "size": "sm",
+                                                            "color": "#999999",
+                                                            "margin": "md",
+                                                            "flex": 0
+                                                          }
+                                                        ]
+                                                      },
+                                                      {
+                                                        "type": "box",
+                                                        "layout": "vertical",
+                                                        "margin": "lg",
+                                                        "spacing": "sm",
+                                                        "contents": [
+                                                          {
+                                                            "type": "box",
+                                                            "layout": "baseline",
+                                                            "spacing": "sm",
+                                                            "contents": [
+                                                              {
+                                                                "type": "text",
+                                                                "text": "Place",
+                                                                "color": "#aaaaaa",
+                                                                "size": "sm",
+                                                                "flex": 1
+                                                              },
+                                                              {
+                                                                "type": "text",
+                                                                "text": "Kaohsiung Six-turtle",
+                                                                "wrap": true,
+                                                                "color": "#666666",
+                                                                "size": "sm",
+                                                                "flex": 5
+                                                              }
+                                                            ]
+                                                          },
+                                                          {
+                                                            "type": "box",
+                                                            "layout": "baseline",
+                                                            "spacing": "sm",
+                                                            "contents": [
+                                                              {
+                                                                "type": "text",
+                                                                "text": "Time",
+                                                                "color": "#aaaaaa",
+                                                                "size": "sm",
+                                                                "flex": 1
+                                                              },
+                                                              {
+                                                                "type": "text",
+                                                                "text": "10:00 - 23:00",
+                                                                "wrap": true,
+                                                                "color": "#666666",
+                                                                "size": "sm",
+                                                                "flex": 5
+                                                              }
+                                                            ]
+                                                          }
+                                                        ]
+                                                      }
+                                                    ]
+                                                  },
+                                                  "footer": {
+                                                    "type": "box",
+                                                    "layout": "vertical",
+                                                    "spacing": "sm",
+                                                    "contents": [
+                                                      {
+                                                        "type": "button",
+                                                        "style": "link",
+                                                        "height": "sm",
+                                                        "action": {
+                                                          "type": "uri",
+                                                          "label": "CALL",
+                                                          "uri": "https://linecorp.com"
+                                                        }
+                                                      },
+                                                      {
+                                                        "type": "button",
+                                                        "style": "link",
+                                                        "height": "sm",
+                                                        "action": {
+                                                          "type": "uri",
+                                                          "label": "WEBSITE",
+                                                          "uri": "https://smiletaiwan.cw.com.tw/article/1327"
+                                                        }
+                                                      },
+                                                      {
+                                                        "type": "spacer",
+                                                        "size": "sm"
+                                                      }
+                                                    ],
+                                                    "flex": 0
+                                                  }
+                                                }])
+
+    default:
+      console.log(`Echo message to ${replyToken}: ${message.text}`);
+      const echo = {
+        type: 'text',
+        text: message.text
+      };
+      return client.replyMessage(replyToken, echo);
+  }
+}
 
       // Request header
       const headers = {
